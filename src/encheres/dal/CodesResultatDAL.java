9cac1488e02be37d1880c4caee31f75fff06787e
Marc <marc.deshayes44@gmail.com>
2020-04-14 16:03:53 +0200

    ajoute la gestion des codes d'erreur, de la connexion à la BDD et commence l'implémentation de la DAO Utilisateur

---
+++ src/encheres/dal/CodesResultatDAL.java
@@ -0,0 +1,10 @@
+package encheres.dal;
+
+public abstract class CodesResultatDAL {
+
+	public static final int INSERT_OBJET_NULL=10000;
+	
+	public static final int INSERT_OBJET_ECHEC=10001;
+	
+	public static final int LECTURE_REPAS_ECHEC=10002;
+}
--- src/encheres/dal/UtilisateurDAOJdbcImpl.java
+++ src/encheres/dal/UtilisateurDAOJdbcImpl.java
@@ -1,5 +1,40 @@
 package encheres.dal;
 
-public class UtilisateurDAOJdbcImpl {
+import encheres.bo.Utilisateur;
 
+public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
+	
+	private static final String INSERT_UTILISATEUR = "insert into utilisateurs(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) values(?,?,?,?,?,?,?,?,?,?,?)";
+
+	@Override
+	public void insert(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Utilisateur select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utilisateur select(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utilisateur select(int noUtilisateur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int noUtilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
